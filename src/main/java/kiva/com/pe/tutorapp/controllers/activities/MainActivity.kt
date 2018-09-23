47ab53074db1c4a549f7ffc6a5675198e66add71
kevinriverosguia <riverosk885@gmail.com>
2018-09-22 22:20:05 -0500

    Integrate Fragments and change titles

--- src/main/java/kiva/com/pe/tutorapp/controllers/activities/MainActivity.kt
+++ src/main/java/kiva/com/pe/tutorapp/controllers/activities/MainActivity.kt
@@ -2,35 +2,52 @@ package kiva.com.pe.tutorapp.controllers.activities
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import kiva.com.pe.tutorapp.R
+import kiva.com.pe.tutorapp.controllers.fragments.FeedFragment
+import kiva.com.pe.tutorapp.controllers.fragments.ProfileFragment
+import kiva.com.pe.tutorapp.controllers.fragments.TutorsFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_tutors)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_profile)
-                return@OnNavigationItemSelectedListener true
-
-            }
-        }
-        false
+        return@OnNavigationItemSelectedListener navigateTo(item)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        supportActionBar?.title = "Feed"
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
+
+    private fun fragmentFor(item: MenuItem):Fragment {
+
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                supportActionBar?.title = getString(R.string.feed_title)
+                return FeedFragment()
+            }
+            R.id.navigation_dashboard -> {
+                supportActionBar?.title = getString(R.string.tutors_title)
+                return TutorsFragment()
+            }
+            R.id.navigation_notifications -> {
+                supportActionBar?.title = getString(R.string.profile_title)
+                return ProfileFragment()
+
+            }
+        }
+        return FeedFragment()
+
+    }
+    private fun navigateTo(item : MenuItem): Boolean{
+        item.setChecked(true)
+        return supportFragmentManager.beginTransaction().replace(R.id.content,fragmentFor(item)).commit()>0
+
+    }
+
 }
--- src/main/java/kiva/com/pe/tutorapp/controllers/fragments/TutorsFragment.kt
+++ src/main/java/kiva/com/pe/tutorapp/controllers/fragments/TutorsFragment.kt
@@ -20,7 +20,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class TutorsFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater,     container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tutors, container, false)
@@ -28,3 +28,4 @@ class TutorsFragment : Fragment() {
 
 
 }
+ 
