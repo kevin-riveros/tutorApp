492cbd0e8bc73846cc90af40171b1b90c18c4797
kevinriverosguia <riverosk885@gmail.com>
2018-09-23 20:20:32 -0500

    integrate login

--- src/main/java/kiva/com/pe/tutorapp/controllers/activities/LoginActivity.kt
+++ src/main/java/kiva/com/pe/tutorapp/controllers/activities/LoginActivity.kt
@@ -12,12 +12,8 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
     }
 
 }
